905d7f303c33528042212e56a5554c09c96cdd0d
zsiki <siki@agt.bme.hu>
2016-02-22 13:55:37 +0100

    optimizing code

--- english/data_processing/code/parabola1.m
+++ english/data_processing/code/parabola1.m
@@ -2,11 +2,12 @@
 xp = [0; 21; 30];
 yp = [0; 55; 75];
 % set up equations
-A = zeros(3, 3);
-l = zeros(3, 1);
-A(:,1) = ones();
-A(:,2) = xp;
-A(:,3) = xp.^2;
+%A = zeros(3, 3);
+%l = zeros(3, 1);
+%A(:,1) = ones();
+%A(:,2) = xp;
+%A(:,3) = xp.^2;
+A = [ones(3,1), xp, xp .^ 2];
 l = yp;
 x = A \ l
 printf('Check\n')
--- english/data_processing/code/parabola_lsm.m
+++ english/data_processing/code/parabola_lsm.m
@@ -12,13 +12,8 @@ n = rows(points);
 if (n < 4 )
 	printf("Few points in input file\n");
 else
-	A = zeros(n, 3);
-	l = zeros(n, 1);
-	A(:, 1) = ones();
-	A(:, 2) = points(:, 1);
-	A(:, 3) = points(:, 1) .^ 2;
+	A = [ones(n, 1), points(:, 1), points(:, 1) .^ 2];
 	l = points(:, 2);
-	x = (A' * A) \ (A' * l)
 	x = A \ l
 	rms = sqrt(sum((polyval(flipud(x), points(:, 1)) - points(:, 2)) .^ 2) / n);
 	printf('RMS = %.3f\n', rms);
