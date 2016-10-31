57bd7892f0aaab6af38feb7734213c34ffe70eb2
zsiki <siki@agt.bme.hu>
2016-10-31 11:21:26 +0100

    adding graphic representation

--- english/data_processing/code/circle.m
+++ english/data_processing/code/circle.m
@@ -6,3 +6,11 @@ a=[x y ones(size(x))]\[-(x.^2+y.^2)];
 xc = -.5*a(1)
 yc = -.5*a(2)
 R  =  sqrt((a(1)^2+a(2)^2)/4-a(3))
+% graphic representation
+plot(x, y, "+r", "markersize", 10);
+hold on;
+xx = linspace(xc-R+0.001, xc+R-0.001, 50);
+yy = sqrt(R^2 - (xx - xc).^2) +yc;
+yyy = -sqrt(R^2 - (xx - xc).^2) +yc;
+plot(xx, yy)
+plot(xx, yyy)
