096d6c91dc12c885501501f628a120db51c4fb87
zsiki <siki@agt.bme.hu>
2016-10-31 11:23:39 +0100

    data input improved

--- english/data_processing/code/parabola_builtin.m
+++ english/data_processing/code/parabola_builtin.m
@@ -1,23 +1,20 @@
 % command line arguments
 args = argv();
-% open input file
-if (length(args))
-	fp = fopen(args{1}, 'r');
-else
-	fp = fopen('parabola.csv', 'r');
-end
 % load all coordinates
-points = sortrows(fscanf(fp, '%f;%f', [2, Inf])');
-n = rows(points);
-if (n < 4 )
-	printf('Few points in input file\n');
-else
-  p = polyfit(points(:, 1), points(:, 2), 2)
-	rms = sqrt(sum((polyval(p, points(:, 1)) - points(:, 2)) .^ 2) / n);
-	printf('RMS = %.3f\n', rms);
-	plot(points(:, 1), points(:, 2), 'o');
-	hold all;
-	plot(points(1, 1):1:points(n, 1), polyval(p, points(1, 1):1:points(n, 1)), '-');
-	legend('base points', 'approx. poly', 'location', 'southeast');
-	hold off;
+fname = 'parabola.csv';
+for i = 1:length(args)
+  if args{i}(1) != '-'
+	  fname = args{1};
+    break;
+  end
 end
+points = dlmread(fname);
+n = rows(points);
+p = polyfit(points(:, 1), points(:, 2), 2)
+rms = sqrt(sum((polyval(p, points(:, 1)) - points(:, 2)) .^ 2) / n);
+printf('RMS = %.3f\n', rms);
+plot(points(:, 1), points(:, 2), 'o');
+hold all;
+plot(points(1, 1):1:points(n, 1), polyval(p, points(1, 1):1:points(n, 1)), '-');
+legend('base points', 'approx. poly', 'location', 'southeast');
+hold off;
