aea9ecacb66ef4e5f9b7ffad38fc5af3999c4c45
zsiki <siki@agt.bme.hu>
2017-05-11 22:35:38 +0200

    point selection vectorized

--- english/data_processing/code/slide.m
+++ english/data_processing/code/slide.m
@@ -7,29 +7,38 @@
 %   tol - tolerance to co-ordinate (default 0.2)
 %   sep - separator in input file (default ',')
 args = argv();  % command line arguments in a cell array
+n = nargin;
+i = 1;
+% skip switches
+if n > 0
+  while i <= nargin && args{i}(1) == '-'
+    i += 1;
+    n -= 1;
+  end
+end
  % check positional parameters
-if nargin > 0
-  fname = args{1};
+if n > 0
+  fname = args{i};
 else
   fname = 'lidar.txt';
 end
-if nargin > 1
-  coo = str2num(args{2});
+if n > 1
+  coo = str2num(args{i+1});
 else
   coo = 1000;
 end
-if nargin > 2
-  col = int32(str2num(args{3}));
+if n > 2
+  col = int32(str2num(args{i+2}));
 else
   col = 3;
 end
-if nargin > 3
-  tol = str2num(args{4});
+if n > 3
+  tol = str2num(args{i+3});
 else
   tol = 0.2;
 end
-if nargin > 4
-  sep = args{5};
+if n > 4
+  sep = args{i+4};
 else
   sep = ',';
 end
@@ -39,9 +48,11 @@ ma = coo + tol / 2;
 lidar = dlmread(fname, sep);
 [r, c] = size(lidar);
 if c >= col
-  for i = 1:r
-    if lidar(i, col) > mi && lidar(i, col) < ma
-      printf('%.3f,%.3f,%.3f\n', lidar(i, 1), lidar(i, 2), lidar(i, 3));
-    end
-  end
+  res = find(lidar(:, col) > mi & lidar(:, col) < ma);
+  printf('%.3f,%.3f,%.3f\n', [lidar(:, 1)(res), lidar(:, 2)(res), lidar(:, 3)(res)]');
+  %for i = 1:r
+  %  if lidar(i, col) > mi && lidar(i, col) < ma
+  %    printf('%.3f,%.3f,%.3f\n', lidar(i, 1), lidar(i, 2), lidar(i, 3));
+  %  end
+  %end
 end
