d6d8a1d8983d3447624ff742e889919e800f0603
zsiki <siki@agt.bme.hu>
2019-02-03 12:37:56 +0100

    use more than 3 points

--- english/data_processing/code/parabola1.m
+++ english/data_processing/code/parabola1.m
@@ -1,13 +1,9 @@
 % coordinates of points
-xp = [0; 21; 30];
-yp = [0; 55; 75];
+xp = [0; 21; 30]
+yp = [0; 55; 75]
 % set up equations
-%A = zeros(3, 3);
-%l = zeros(3, 1);
-%A(:,1) = ones();
-%A(:,2) = xp;
-%A(:,3) = xp.^2;
-A = [ones(3,1), xp, xp .^ 2];
+n = rows(xp);
+A = [ones(n,1), xp, xp .^ 2];
 l = yp;
 x = A \ l
 printf('Check\n')
