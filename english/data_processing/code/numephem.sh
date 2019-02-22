1f471af0883548010c5cfe201b9c835d3d3dd434
zsiki <siki@agt.bme.hu>
2019-02-22 14:16:49 +0100

    extended to handle command line parameter

--- english/data_processing/code/numephem.sh
+++ english/data_processing/code/numephem.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-
-for file in "brdm"[0-3][0-9][0-9]"0."[0-9][0-9]"p"
-do
-	awk -f numephem.awk $file
-done
+# process all files in current directory or files given by parameters
+if [ $# -eq 0 ]
+then
+	for file in brdm[0-3][0-9][0-9]0.[0-9][0-9]p
+	do
+		awk -f numephem.awk $file
+	done
+else
+	for file in "$@"
+	do
+		awk -f numephem.awk $file
+	done
+fi
