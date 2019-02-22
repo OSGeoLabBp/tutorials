420dc6180e0f4fc7214addea861d1ead6c872508
zsiki <siki@agt.bme.hu>
2019-02-22 15:01:37 +0100

    comments added

--- english/data_processing/code/numephem.sh
+++ english/data_processing/code/numephem.sh
@@ -2,11 +2,13 @@
 # process all files in current directory or files given by parameters
 if [ $# -eq 0 ]
 then
+	# process all rinex file in current directory
 	for file in brdm[0-3][0-9][0-9]0.[0-9][0-9]p
 	do
 		awk -f numephem.awk $file
 	done
 else
+	# process rinex files given in the command line
 	for file in "$@"
 	do
 		awk -f numephem.awk $file
