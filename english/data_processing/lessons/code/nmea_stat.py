cca73bb2aad20e256e799c1bc1ea3c5a2d14163b
zsiki <siki1958@gmail.com>
2021-02-17 13:04:35 +0100

    checksum fixed for one digit output

--- english/data_processing/lessons/code/nmea_stat.py
+++ english/data_processing/lessons/code/nmea_stat.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 """ NMEA statistics
 """
 
@@ -5,21 +7,25 @@ import sys
 import re
 
 def checksum(buf):
-    """ check nmea checksum on line """
+    """ check nmea checksum on line
+        returns two digit hexa number
+    """
     cs = ord(buf[1])
     for ch in buf[2:-3]:
         cs ^= ord(ch)
-    return cs
+    ch = '0' + re.sub('^0x', '', hex(cs))
+    return ch[-2:].upper()
 
 def nmea2deg(nmea):
     """ convert nmea angle (dddmm.ss) to degree """
     w = nmea.rstrip('0').split('.')
     return int(w[0][:-2]) + int(w[0][-2:]) / 60.0 + int(w[1]) / 3600.0
     
-fin = 'nmea2.txt'
 if len(sys.argv) > 1:
     fin = sys.argv[1]   # get input file from command line
-fi = open(fin, 'r') # input file
+    fi = open(fin, 'r') # input file
+else:
+    fi = sys.stdin
 token = {}
 minlat = 90
 minlon = 180
@@ -27,7 +33,7 @@ maxlat = -90
 maxlon = -180
 for line in fi:
     line = line.strip()
-    if hex(checksum(line))[2:].upper() != line[-2:]:
+    if checksum(line) != line[-2:]:
         print("Chechsum error: " + line)
         continue
     nmea = line.split(',')
@@ -43,14 +49,10 @@ for line in fi:
             if nmea[5].upper() == 'W':
                 lon = 360 - lon
             height = float(nmea[9])
-            if lat < minlat:
-                minlat = lat
-            if lon < minlon:
-                minlon = lon
-            if lat > maxlat:
-                maxlat = lat
-            if lon > maxlon:
-                maxlon = lon
+            minlat = min(minlat, lat)
+            minlon = min(minlon, lat)
+            maxlat = max(maxlat, lat)
+            maxlon = max(maxlon, lon)
 fi.close()
 print (minlat, minlon, maxlat, maxlon)
 for t in token:
