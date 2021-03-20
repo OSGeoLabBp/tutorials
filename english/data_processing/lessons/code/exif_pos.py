5de5b544d93b12c3e544f0deafdfb2742c73ae59
zsiki <siki1958@gmail.com>
2021-03-20 17:28:15 +0100

    wildcard handlin in command line for windows

--- english/data_processing/lessons/code/exif_pos.py
+++ english/data_processing/lessons/code/exif_pos.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
+""" Collect GPS position from jpg files """
+
+import platform
+import glob
+import sys
 import PIL.Image
 import PIL.ExifTags
-import sys
 
-def to_degrees(dir, value):
+def to_degrees(dire, value):
     """
     convert the GPS coordinates stored in the EXIF to degress in float format
     :param value: tuples of DMS
@@ -15,7 +19,7 @@ def to_degrees(dir, value):
     d = float(value[0][0]) / float(value[0][1])
     m = float(value[1][0]) / float(value[1][1])
     s = float(value[2][0]) / float(value[2][1])
-    w = 1 if dir in ('E', 'N') else -1
+    w = 1 if dire in ('E', 'N') else -1
     return w * (d + (m / 60.0) + (s / 3600.0))
 
 def img_pos(name):
@@ -23,11 +27,10 @@ def img_pos(name):
     get GPS position from image
     :param name: image path
     :returns: tuple of (lat, lon)
-    """ 
+    """
     img = PIL.Image.open(name)
-    exif_data = {
-        PIL.ExifTags.TAGS[k]: v for k, v in img._getexif().items()
-            if k in PIL.ExifTags.TAGS }
+    exif_data = {PIL.ExifTags.TAGS[k]: v for k, v in img._getexif().items()
+                 if k in PIL.ExifTags.TAGS}
     if 'GPSInfo' not in exif_data:
         return None
     return (to_degrees(exif_data['GPSInfo'][3], exif_data['GPSInfo'][4]),
@@ -37,8 +40,14 @@ if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Usage: {} image_file(s)".format(sys.argv[0]))
         exit(-1)
+    if platform.system().lower() == "windows":
+        names = []
+        for arg in sys.argv[1:]:
+            names += glob.glob(arg)
+    else:
+        names = sys.argv[1:]
     # process parameters
-    for name in sys.argv[1:]:
+    for name in names:
         p = img_pos(name)
         if p:
             print("{},{:.6f},{:.6f}".format(name, p[0], p[1]))
